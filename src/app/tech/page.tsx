c36651a2e347716949cf0bc05330b0f9868b3e2c
404salad <404salad@gmail.com>
2024-04-10 02:36:38 +0530

    added component

---
+++ src/app/tech/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import Image from 'next/image';
+export const metadata: Metadata = {
+  title: "Technician Page",
+  description:
+    "This is Next.js Calender page for TailAdmin  Tailwind CSS Admin Dashboard Template",
+};
+
+const CalendarPage = () => {
+  return (
+    <DefaultLayout>
+<div></div>
+    </DefaultLayout>
+  );
+};
+
+export default CalendarPage;
