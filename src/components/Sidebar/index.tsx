a89c4cf360f2354bdd5e505ed77fe2f5c9319769
404salad <404salad@gmail.com>
2024-04-10 05:49:43 +0530

    removed sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -61,12 +61,12 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   return (
     <aside
       ref={sidebar}
-      className={`absolute left-0 top-0 z-9999 flex h-screen w-30 text-xs flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
+      className={`absolute hidden left-0 top-0 z-9999 flex h-screen w-30 text-xs flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
         sidebarOpen ? "translate-x-0" : "-translate-x-full"
       }`}
     >
       {/* <!-- SIDEBAR HEADER --> */}
-      <div className=" flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
+      <div className="hid flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
         <Link href="/">
           {/* <Image
             width={176}
